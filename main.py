7a0900283500b2ccef0e556d3d208cfaa280621e
johann-su <johann123.js@gmail.com>
2021-02-24 09:16:25 +0100

    cleaned up data preprocessing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 # import geoplot as gplt
 import argparse
-from datetime import datetime
+from datetime import datetime, date
 from collections.abc import Iterable
 
 # Parser for arguments
@@ -13,7 +13,7 @@ parser.add_argument('-c', '--chart', type=str,
                     help='The chart type to plot')
 parser.add_argument('-r', '--range', type=str,
                     help='The date range for the Chart')
-parser.add_argument('--state', type=str, default="Sachsen",
+parser.add_argument('--state', type=str, required=False,
                     help='The State to look at')
 parser.add_argument('--county', type=str, required=False,
                     help='The county to look at')
@@ -23,78 +23,55 @@ args = parser.parse_args()
 # Read data
 df = pd.read_csv('./data/covid_de.csv')
 df['date'] = pd.to_datetime(df['date'])
-df = df.sort_values(by='date', ascending=False)
 
 demographics = pd.read_csv('./data/demographics_de.csv')
 
 # Function to plot different types of charts
 def plot(data, chart, range, state):
+    # drop unwanted data
+    tf = df.drop(['age_group', 'gender'], axis=1)
 
-    # days in specified timeframe
-    timedelta = pd.Timedelta(range).days
+    # filter dataset for entries in range
+    td = pd.Timedelta(range)
+    tf = tf[tf['date'] > datetime.now() - td]
 
-    aggregation_functions = {'state': 'first', 'cases': 'sum', 'deaths': 'sum', 'recovered': 'sum'}
-    
-    # drop useless columns and filter for state
-    tf = df.drop(['county', 'age_group', 'gender'], axis=1).loc[lambda df: df['state'] == args.state]
-    # combine values for dates
-    tf = tf.groupby(tf['date']).aggregate(aggregation_functions)
+    # for Germany
+    if state == None:
+        tf = tf.drop(['state', 'county'], axis=1)
 
-    # make index numerical
-    tf['date'] = tf.index
-    tf = tf.reset_index(drop=True)
+        aggregation_functions = {'cases': 'sum', 'deaths': 'sum', 'recovered': 'sum'}
 
-    if data == 'infections':
-        # filter for the time specified in timeframe
-        tf = tf.loc[:timedelta]
-    elif data == 'incidence':
-        # calculate 7 day incidence
-        tf = tf.loc[:6]
+        tf = tf.groupby(tf['date']).aggregate(aggregation_functions)
 
-        dm = demographics.drop(['gender', 'age_group'], axis=1).loc[lambda df: demographics['state'] == args.state]
-        dm = dm.groupby(dm['state']).aggregate({'state': 'first', 'population': 'sum'})
+    # for a state
+    elif county == None:
+        print(tf)
+        tf = tf.drop(['county'], axis=1)
 
-        dm['date'] = dm.index
-        dm = dm.reset_index(drop=True)
+        aggregation_functions = {'state': 'first', 'cases': 'sum', 'deaths': 'sum', 'recovered': 'sum'}
 
-        incidence = float(tf.aggregate({'cases': 'sum'})['cases'] / dm['population'] * 100000)
+        tf = tf.loc[lambda df: df['state'] == state]
+        tf = tf.groupby(tf['date']).aggregate(aggregation_functions)
+
+    # for a county
+    else:
+        aggregation_functions = {'state': 'first', 'county': 'first', 'cases': 'sum', 'deaths': 'sum', 'recovered': 'sum'}
+        tf = tf.loc[lambda df: df['state'] == state]
+        tf = tf.loc[lambda df: df['county'] == county]
+        tf = tf.groupby(tf['date']).aggregate(aggregation_functions)
 
     # plot the Graph
-    if chart == 'line':
-        if data == 'infections':
-            x = tf['date']
-            y_inf = tf['cases']
-            y_death = tf['deaths']
+    x = tf['date']
+    y_inf = tf['cases']
+    y_death = tf['deaths']
 
-            plt.plot(x, y_inf, label="Infections", color="b")
-            plt.plot(x, y_death, label="Deaths", color="r")
+    plt.plot(x, y_inf, label="Infections", color="b")
+    plt.plot(x, y_death, label="Deaths", color="r")
 
-            plt.title(f'Infections / Deaths over the last {range}')
-
-        elif data == 'incidence':
-            # TODO: Better way to visulize incidence
-           print(f"The seven day incidence for {state} is {incidence}")
-
-    elif chart == 'bar':
-        if data == 'infections':
-            x = tf['date']
-            y_inf = tf['cases']
-            y_death = tf['deaths']
-
-            plt.bar(x, y_inf, label="Infections", color="b")
-            plt.bar(x, y_death, label="Deaths", color="r")
-
-            plt.title(f'Infections / Deaths over the last {range}')
-
-        elif data == 'incidence':
-            # TODO: Better way to visulize incidence
-           print(f"The seven day incidence for {state} is {incidence}")
-
-    elif chart == 'map':
-        if data == 'infections':
+    plt.title(f'Infections / Deaths over the last {range}')
 
     plt.legend()
     plt.savefig(f'./charts/{chart}_{state}_{range}_{datetime.now()}.png')
-    # plt.show()
+    plt.show()
 
 plot(args.data, args.chart, args.range, args.state)
