36c8b26a4efce4397434c056e00b59401b8af8f7
johann-su <johann123.js@gmail.com>
2021-03-11 21:21:34 +0100

    added setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-apt-get update
-apt-get install gdal-bin libgdal-dev libatlas-base-dev cmake make -y
+sudo apt-get update
+sudo apt-get install gdal-bin libgdal-dev libatlas-base-dev cmake make -y
 
-apt-get install sqlite3
+sudo apt-get install sqlite3
 mkdir tmp
 cd tmp
 wget https://download.osgeo.org/proj/proj-6.3.2.tar.gz
@@ -11,8 +11,10 @@ cd proj-6.3.2
 make
 make install
 ldconfig
+cd ../
+rm -r tmp
 
-mkdir libspatialindex && \
+mkdir libspatialindex
 cd libspatialindex/
 wget https://github.com/libspatialindex/libspatialindex/releases/download/1.9.3/spatialindex-src-1.9.3.tar.gz
 tar -xvf spatialindex-src-1.9.3.tar.gz
@@ -21,6 +23,8 @@ cmake -DCMAKE_INSTALL_PREFIX=/usr/local .
 make
 make install
 ldconfig
+cd ../
+rm -r libspatialindex
 
 pip install venv
 python venv env
