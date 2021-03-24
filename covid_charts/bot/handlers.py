b07092bd46b2291d82fc98b3a7b68075d7a254b1
johann-su <johann123.js@gmail.com>
2021-03-24 17:28:06 +0100

    latest changes

--- covid_charts/bot/handlers.py
+++ covid_charts/bot/handlers.py
@@ -10,7 +10,7 @@ from covid_charts.bot import setup_conv
 from covid_charts.bot import bot
 from covid_charts.charts import Chart
 
-import tweepy
+from covid_charts import collect_news
 
 setup_handler = ConversationHandler(
     name='setup_handler',
@@ -99,16 +99,10 @@ def status(update: Update, context: CallbackContext) -> None:
     update.message.reply_text(f"New infections in Germany: {germany['cases'].values[0]}\nNew infections in Saxony: {state.loc[state.index=='Sachsen']['cases'].values[0]}") 
 
 def news(update: Update, context: CallbackContext) -> None:
-    auth = tweepy.OAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_SECRET'))
-    auth.set_access_token(os.getenv('TWITTER_AT'), os.getenv('TWITTER_ATS'))
+    zeit = collect_news.get_articles()
+    print(zeit)
 
-    api = tweepy.API(auth)
-
-    public_tweets = api.user_timeline('@rki_de', count=10)
-
-    id = random.randint(0, 10)
-
-    update.message.reply_text(f'Twitter sagt:\n{public_tweets[id].text}\n\nhttps://twitter.com/{public_tweets[id].user.screen_name}/status/{public_tweets[id].id}')
+    update.message.reply_text(f'Hier sind ein paar interessante Artikel aus der Zeit zu Covid:\n')
 
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id,
--- covid_charts/collect_news.py
+++ covid_charts/collect_news.py
@@ -1,22 +1,24 @@
 import os
 import random
 
+import json
 import requests
 import tweepy
 
-# http://developer.zeit.de/
-# TODO: Insert API Key
-# zeit_news = requests.get('http://api.zeit.de/content?q=covid', headers={'X-Authorization': os.getenv('ZEIT_API_KEY')}).text
+def get_articles():
+    # http://developer.zeit.de/
+    zeit_news = requests.get('http://api.zeit.de/content?q=covid', headers={'X-Authorization': os.getenv('ZEIT_API_KEY')}).text
 
-# print(zeit_news)
+    zeit_news = json.loads(zeit_news)
 
-auth = tweepy.OAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_SECRET'))
-auth.set_access_token(os.getenv('TWITTER_AT'), os.getenv('TWITTER_ATS'))
+    return zeit_news['matches']
 
-api = tweepy.API(auth)
+def get_tweets():
+    auth = tweepy.OAuthHandler(os.getenv('TWITTER_KEY'), os.getenv('TWITTER_SECRET'))
+    auth.set_access_token(os.getenv('TWITTER_AT'), os.getenv('TWITTER_ATS'))
 
-public_tweets = api.user_timeline('@rki_de', count=10) # @c_drosten, 
+    api = tweepy.API(auth)
 
-id = random.randint(0, 10)
+    public_tweets = api.user_timeline('@rki_de', count=10) # @c_drosten, 
 
-print(public_tweets[id])
+    return public_tweets
